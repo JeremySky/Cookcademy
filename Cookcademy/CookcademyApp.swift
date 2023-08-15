aea79216b500e068ee6491b830fea48810f8c83c
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-08-15 15:24:58 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Jeremy Manlangit on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
