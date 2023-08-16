0daec57425ed56f53072dfdda782aa3e18775dbf
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-08-16 10:57:50 -0700

    add color file and color properties to views

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -11,12 +11,17 @@ struct RecipesListView: View {
     
     @StateObject var recipeData = RecipeData()
     
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(recipes) { recipe in
                     NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
                 }
+                .listRowBackground(listBackgroundColor)
+                .foregroundColor(listTextColor)
             }
         }
         .navigationTitle(navigationTitle)
