e1814fa3f66118b9b04b88f0938b8d4d3dc1f53b
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-08-17 23:22:59 -0700

    add ModifyDirectionView framing

--- Cookcademy/Views/ModifyRecipes/ModifyDirectionView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyDirectionView.swift
@@ -8,25 +8,43 @@
 import SwiftUI
 
 struct ModifyDirectionView: ModifyComponentView {
-    
-    @Binding var direction: Direction
-    let createAction: ((Direction) -> Void)
+  @Binding var direction: Direction
+  let createAction: (Direction) -> Void
     
     init(component: Binding<Direction>, createAction: @escaping (Direction) -> Void) {
         self._direction = component
         self.createAction = createAction
     }
-    
-    var body: some View {
-        Color(.red)
+  
+  private let listBackgroundColor = AppColor.background
+  private let listTextColor = AppColor.foreground
+  
+  @Environment(\.presentationMode) private var mode
+  
+  var body: some View {
+    Form {
+      TextField("Direction Description", text: $direction.description)
+        .listRowBackground(listBackgroundColor)
+      Toggle("Optional", isOn: $direction.isOptional)
+        .listRowBackground(listBackgroundColor)
+      HStack {
+        Spacer()
+        Button("Save") {
+          createAction(direction)
+          mode.wrappedValue.dismiss()
+        }
+        Spacer()
+      }.listRowBackground(listBackgroundColor)
     }
+    .foregroundColor(listTextColor)
+  }
 }
 
 struct ModifyDirectionView_Previews: PreviewProvider {
-    @State static var newDirection = Direction()
-    static var previews: some View {
-        ModifyDirectionView(component: $newDirection) { direction in
-            print(direction)
-        }
+  @State static var emptyDirection = Direction(description: "", isOptional: false)
+  static var previews: some View {
+    NavigationView {
+      ModifyDirectionView(component: $emptyDirection) { _ in return }
     }
+  }
 }
